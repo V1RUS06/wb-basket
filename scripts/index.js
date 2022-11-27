184b01e5d9fbfc960f75c13bff82cd234a28beab
Denis <denison4@mail.ru>
2022-11-27 13:50:36 +0300

    доделал логику с выбором карты

--- scripts/index.js
+++ scripts/index.js
@@ -4,10 +4,11 @@ import {Payment} from "./payment.js";
 
 window.addEventListener('load', () => {
   Cart.renderCart()
-  Payment.renderPayment()
   rerenderFinalPrice() // Начальное обновление цены
   addListenerOnCheckboxes();
   addListenerOnProductCounter();
+  Payment.renderPayment()
+  addModalsListeners();
 })
 
 function addListenerOnProductCounter() {
@@ -43,6 +44,57 @@ function addListenerOnProductCounter() {
   })
 
 }
+
+function addModalsListeners(){
+
+  const changeButtons = document.querySelectorAll('.change-btn')
+  changeButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+      const action = e.target.dataset.action
+
+      if(action ==='payment-modal'){
+        Payment.renderPaymentModal()
+        addPaymentModalListeners()
+
+
+
+      }
+    })
+  })
+
+}
+
+function addPaymentModalListeners() {
+  const closeBtn = document.querySelector('.close_btn')
+  const checkboxesLabel = document.querySelectorAll('.circle_checkbox')
+  const chooseBtn = document.querySelector('.modal_button')
+
+
+  closeBtn.addEventListener('click', e => {
+    closePaymentModal()
+  })
+  checkboxesLabel.forEach(elem => {
+    elem.addEventListener('click', e => {
+      const index = e.currentTarget.dataset.index
+      Payment.changeCardInState(index)
+    })
+  })
+
+  chooseBtn.addEventListener('click', () => {
+    rerenderCard()
+  })
+}
+
+
+function closePaymentModal(){
+  const modal = document.querySelector('.modal')
+  const overlay = document.querySelector('.overlay')
+
+  modal.remove()
+  overlay.remove()
+}
+
+
 
 function addListenerOnCheckboxes() {
   const checkboxes = document.querySelectorAll('.check_box');
@@ -101,4 +153,22 @@ function changeQuantityBasedPrice(id) {
   mobilePrice.textContent = `${newPrice} сом`;
   priceWithoutDiscount.textContent = `${lastPrice} сом`;
   mobilePriceWithoutDiscount.textContent = `${lastPrice} сом`;
+}
+
+function rerenderCard() {
+  const cardSpan = document.querySelectorAll('.card-span')
+  const cardImg = document.getElementById('img_card_on_payment_container')
+  const cardImgMobile = document.getElementById('img_card_on_payment_mobile_container')
+  const cardImgOnBasket = document.getElementById('img_card_on_payment_basket_container')
+  const index = Payment.selectedCardIndex
+
+  console.log(Payment.cards[index].img_source)
+
+  cardSpan.forEach(elem => {
+    elem.textContent = Payment.cards[index].card_number + " " + Payment.cards[index].card_day
+  })
+  cardImg.src = Payment.cards[index].img_source
+  cardImgOnBasket.src = Payment.cards[index].img_source
+  cardImgMobile.src = Payment.cards[index].img_source
+  closePaymentModal()
 }
--- scripts/payment.js
+++ scripts/payment.js
@@ -9,63 +9,78 @@ export const Payment = {
     {
       id: 2,
       img_source: '/assets/icons/visa_card.svg',
-      card_number: '1234 56•• •••• 1234',
+      card_number: '1234 56•• •••• 2222',
       card_day: '01/30',
     },
     {
       id: 3,
       img_source: '/assets/icons/master_card.svg',
-      card_number: '1234 56•• •••• 1234',
+      card_number: '1234 56•• •••• 3333',
       card_day: '01/30',
     },
     {
       id: 4,
       img_source: '/assets/icons/maestro_card.svg',
-      card_number: '1234 56•• •••• 1234',
+      card_number: '1234 56•• •••• 4444',
       card_day: '01/30',
     },
   ],
   selectedCardIndex: 0,
 
-  renderPayment(){
-    const payment = `
-    <div class="modal">
+  changeCardInState(index){
+    this.selectedCardIndex = index
+  },
+
+  renderPaymentModal(){
+    const cards = this.cards.map((card, index) => {
+      return`<div class="cards_container">
+            <label class="circle_checkbox" data-index="${index}">
+              <input ${index === this.selectedCardIndex ? 'checked' : ''} data-id="${card.id}" ${this.selectedCardIndex === index ? 'checked' : ''} class="check_input" type="radio" name="radio-btn">            
+              <span class="circle_check_box"></span>
+              <div class="bank_card card_margin pl-10">
+              <img src="${card.img_source}" alt="bank_card" class="card_img">
+                ${card.card_number}
+            </div>
+            </label>
+            
+          </div>`}).join('')
+
+    const modal = `
+     <div class="modal">
       <div class="modal_header">
         <div class="modal_title">Способ оплаты</div>
-        <div class="close">
+        <div class="close_btn">
           <img class="close_img" src="/assets/icons/close.svg" alt="close">
         </div>
       </div>
       
       <div class="content">
-        ${this.cards.map((card, index) => {
-          return`<div class="cards_container">
-            <label class="circle_checkbox">
-              <input data-id="${card.id}" ${this.selectedCardIndex === index ? 'checked' : ''} class="check_input" type="checkbox">            <span class="circle_check_box"></span>
-            </label>
-            <div class="bank_card card_margin pl-10">
-              <img src="${card.img_source}" alt="bank_card">
-                ${card.card_number}
-            </div>
-          </div>`})}
+        ${cards}
       </div>
       
       <button class="modal_button">Выбрать</button>
 
     </div>
     
-    <div class="overlay"></div>
-    
+    <div class="overlay"></div>`
+
+    const container = document.querySelector('.payment_container')
+
+    container.insertAdjacentHTML('afterend', modal)
+  },
+
+  renderPayment(){
+    const payment = `    
     <div class="white_wrapper">
       <div class="white_wrapper_header">
         <div class="white_wrapper_title">Способ оплаты</div>
-        <div class="white_wrapper_change_btn">Изменить</div>
+        <div data-action="payment-modal" class="white_wrapper_change_btn change-btn">Изменить</div>
       </div>
 
       <div class="bank_card">
-        <img src="${this.cards[this.selectedCardIndex].img_source}" alt="bank_card">
-              ${this.cards[this.selectedCardIndex].card_number}
-        <span>${this.cards[this.selectedCardIndex].card_day}</span>
+        <img id="img_card_on_payment_container" src="${this.cards[this.selectedCardIndex].img_source}" alt="bank_card">
+              
+        <span class="card-span">${this.cards[this.selectedCardIndex].card_number} ${this.cards[this.selectedCardIndex].card_day}</span>
       </div>
 
       <div class="comment">
